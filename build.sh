4426bac76e0aacc80532973e7538e8c94d72d3e5
Vihaan Sapra <vihaansapra@gail.com>
2025-02-19 02:35:27 +0530

    script for building

---
+++ build.sh
@@ -0,0 +1,5 @@
+cd /root/CrazySim/ros2_ws
+colcon build --symlink-install --merge-install --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS=ON --packages-select icuas25_competition
+source /root/CrazySim/ros2_ws/install/setup.bash
+cd /root/CrazySim/ros2_ws/src/icuas25_competition/startup
+./start.sh
