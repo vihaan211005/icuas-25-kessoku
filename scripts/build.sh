4d3bb58b88a1ee6dff5b5ef0b618621bf8490c12
Pulak Gautam <pulak.gautam@gmail.com>
2025-03-01 17:30:19 +0530

    fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 build(){
-    cd /root/CrazySim/ros2_ws && \
-    colcon build --symlink-install --merge-install --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS=ON --packages-select icuas25_competition 2> out || { exit 1; }
+    cd /root/CrazySim/ros2_ws
+    colcon build --symlink-install --merge-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --packages-select icuas25_competition 2>&1 | tee out
 
-    source /root/CrazySim/ros2_ws/install/setup.bash && \
-    cd /root/CrazySim/ros2_ws/src/icuas25_competition/startup && \
-    ./start.sh
+    flag="$(cat out | grep -i error | wc -l)"
+    if [ "$flag" -eq 0 ]; then
+        source /root/CrazySim/ros2_ws/install/setup.bash
+        cd /root/CrazySim/ros2_ws/src/icuas25_competition/startup
+        ./start.sh
+    else
+        echo "Build failed"
+    fi
 }
 
