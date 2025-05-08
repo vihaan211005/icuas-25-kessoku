1e7a2c0f716eb49d0ac15b8cd2f2cb05fe7cd765
KhAlamdar11 <75181406+KhAlamdar11@users.noreply.github.com>
2025-05-09 00:06:39 +0200

    fix(aideck_streamer_tcp): retry initial connection and clean up threads on reconnect feat: add aideck_streamer_udp

---
+++ icuas25_competition/launch/aideck_streamer_tcp.launch.py
@@ -0,0 +1,16 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    aideck_config_path = os.path.join(get_package_share_directory('icuas25_competition'),
+                                                                  'config',
+                                                                  'aideck_streamer.yaml')
+    launch_description = []
+    launch_description.append(Node(
+                                package="icuas25_competition",
+                                executable="aideck_streamer_tcp.py",
+                                name="aideck_streamer_tcp",
+                                parameters=[{"aideck_config_path": aideck_config_path}]))
+    return LaunchDescription(launch_description)
